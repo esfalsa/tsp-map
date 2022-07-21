1476dc96017b62aec7ffda4b7c4a506ceb5e95a8
Pronoun <pronoun.nationstates@gmail.com>
2022-07-20 20:51:39 -0700

    Initial commit

---
+++ src/routes/index.svelte
@@ -0,0 +1,25 @@
+<script>
+	import Map from '../components/map.svelte';
+	import { data } from '$lib/data.js';
+
+	let selectedPlot = null;
+	$: selected = data[selectedPlot];
+</script>
+
+<h1 class="text-3xl font-bold">Map of the South Pacific</h1>
+
+<div class="flex flex-row">
+	<div class="flex-1">
+		<Map bind:selectedPlot />
+	</div>
+	<div class="flex-1">
+		<div id="infoDisplay">
+			{#if selected}
+				<h3 class="text-3xl font-extrabold" id="name">{selected.name}</h3>
+				<p><strong>Plot Number:</strong> {selectedPlot}</p>
+			{:else if selectedPlot}
+				<p><strong>Plot Number:</strong> {selectedPlot}</p>
+			{/if}
+		</div>
+	</div>
+</div>
